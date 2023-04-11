3ce1e4cfc25daf7a283771a3c8b1f7854a982ceb
Tomas Cuevas <memo_mister398@hotmail.com>
2023-04-10 22:46:04 -0300

    increment method with set property

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import "./App.css";
 
 function App() {
   const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
 
   return (
     <div>
       <h1>Counter: {count}</h1>
+      <button onClick={() => increment(10)}>Increment by 10</button>
     </div>
   );
 }
--- src/store/counter.store.ts
+++ src/store/counter.store.ts
@@ -3,8 +3,13 @@ import create from "zustand";
 //* interface *//
 interface CounterState {
   count: number;
+  increment(value: number): void;
 }
 
-export const useCounterStore = create<CounterState>(() => ({
+export const useCounterStore = create<CounterState>((set, get) => ({
   count: 10,
+  increment: (value: number) =>
+    set((state) => ({
+      count: state.count + value,
+    })),
 }));
